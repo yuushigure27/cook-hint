0d442ecf6cdb0d7cdef294103a0a6930ec6c04a9
yuushigure27 <yuushigure1227@gmail.com>
2024-05-02 13:47:30 +0000

    [fix] user login signup

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -54,11 +54,11 @@ class User::RegistrationsController < Devise::RegistrationsController
   # The path used after sign up.
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    mypage_path
+    users_my_page_path
   end
 
   def after_update_path_for(resource)
-    mypage_path
+    users_my_page_path
   end
 
   # The path used after sign up for inactive accounts.
