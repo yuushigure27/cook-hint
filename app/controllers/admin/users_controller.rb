8266103d0ab719083075ef403576802b23033437
yuushigure27 <yuushigure1227@gmail.com>
2024-05-08 15:09:47 +0000

    [add]view/admin/users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,10 +14,15 @@ class Admin::UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    if @user.update(customer_params)
+    if @user.update(user_params)
       redirect_to admin_user_path(@user), notice: "ユーザー情報を更新しました。"
     else
       render :edit
     end  
   end
+  
+  def user_params
+  params.require(:user).permit(:name, :email, :is_active)
+  end
+  
 end
