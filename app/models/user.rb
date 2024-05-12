148708ae25b40a58b9e3cf1e51a2d7c6d6e3e347
yuushigure27 <yuushigure1227@gmail.com>
2024-05-12 04:47:44 +0000

    [fix]mypage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_one_attached :profile_image
-  
+
   validates :email, presence: true, uniqueness: true
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
-  
-  def introduction
-    
-  end
-  
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.png'
   end
-  
+
   GUEST_USER_EMAIL = "guest@example.com"
 
   def self.guest
@@ -24,7 +20,7 @@ class User < ApplicationRecord
       user.name = "guestuser"
     end
   end
-  
+
   def guest_user?
     email == GUEST_USER_EMAIL
   end
