19b41bcfc02bfcfb74b2059419bb6983cc95d644
yuushigure27 <yuushigure1227@gmail.com>
2024-05-13 03:40:27 +0000

    [fix] user editpage

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -19,16 +19,21 @@ class User::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: 'User information updated successfully.'
     else
-      render "edit"
+      render :edit
     end
   end
-
 
   def ensure_guest_user
     @user = User.find(params[:id])
     return unless @user.guest_user?
     redirect_to users_my_page_path, alert: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_image, :introduction)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, length: { maximum: 50 }
+  validates :introduction, length: { maximum: 100 }
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.png'
@@ -20,6 +20,8 @@ class User < ApplicationRecord
     end
   end
 
+  GUEST_USER_EMAIL = 'guest@example.com'.freeze
+  
   def guest_user?
     email == GUEST_USER_EMAIL
   end
