87e7949453be8c6c7b4864a3a0005d06124ba34d
yuushigure27 <yuushigure1227@gmail.com>
2024-05-17 19:02:46 +0000

    [add]admin post/show index page

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,13 +1,23 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
-  end
-
-  def edit
+    @genres = Genre.all
+    @posts = Post.page(params[:page]).per(10)
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 
-  def new
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path, notice: "投稿を削除しました"
+    else
+      flash[:alert] = "削除に失敗しました"
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
