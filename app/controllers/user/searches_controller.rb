97e3adf8c2dd7ea8fc7282560a2a8b4bd9d1465f
yuushigure27 <yuushigure1227@gmail.com>
2024-05-17 13:31:40 +0000

    [fix]admin

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -1,5 +1,18 @@
 class User::SearchesController < ApplicationController
-
+  
+  
+   def search
+    @model = params[:model]
+    @content = params[:content]
+    @method = params[:method]
+    
+    # 選択したモデルに応じて検索を実行
+    if @model  == "post"
+      @records = Post.search_for(@content, @method)
+    else
+      @records = Genre.search_for(@content, @method)
+    end
+  end
 
   def genre_search
     @genre = Genre.find(params[:genre_id])
