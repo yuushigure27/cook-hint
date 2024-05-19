3bcc56beecb8b41cc2210c2a552d13f5ec67d165
yuushigure27 <yuushigure1227@gmail.com>
2024-05-19 05:48:12 +0000

    [fix]genre layout

--- app/controllers/user/likes_controller.rb
+++ app/controllers/user/likes_controller.rb
@@ -4,18 +4,18 @@ class User::LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     post.likes.create(user: current_user)
-    redirect_to post
+    redirect_back(fallback_location: "/posts")
   end
 
   def destroy
     post = Post.find(params[:post_id])
     like =current_user.likes.find_by(post_id: post.id)
     like.destroy if like
-    redirect_to post
+    redirect_back(fallback_location: "/posts")
   end
 
   def index
     @likes = current_user.likes.includes(:post)
-    @likes_genre = @likes.where(posts: { genre_id: params[:genre_id] }) if params[:genre_id].present?
+    @posts =Post.where(id: @likes.pluck(:post_id))
   end
 end
