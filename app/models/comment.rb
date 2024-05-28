24faf2ccb07b4170f683c80ab01426d23cc8798b
yuushigure27 <yuushigure1227@gmail.com>
2024-05-28 02:45:55 +0000

    [fix]comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  
-  validates :content, presence: true, length: { maximum: 300 }
+
+  validates :content, presence: true
 end
