07160dfa356162f46d11201773180546cd630c57
yuushigure27 <yuushigure1227@gmail.com>
2024-06-16 05:13:44 +0000

    [update]admin_comment destroy

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,12 +1,18 @@
 class Admin::CommentsController < ApplicationController
   before_action :check_guest_user, only: [:create]
+  
 
+  
   
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
+    if @comment.best_answer
+      @comment.update(best_answer: false)
+    end
+
     @comment.destroy
-     redirect_to admin_post_path(@post), notice: 'コメントが削除されました'
+    redirect_to admin_post_path(@post), notice: "コメントを削除しました。"
   end
 
 
